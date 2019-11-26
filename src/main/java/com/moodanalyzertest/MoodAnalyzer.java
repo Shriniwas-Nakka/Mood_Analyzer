5c5ff978db6d881deee47c6cf26b459bd826e877
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-26 10:22:53 +0530

    ADDED: functionality to analyse message and respond to SAD

--- src/main/java/com/moodanalyzertest/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzertest/MoodAnalyzer.java
@@ -1,4 +1,11 @@
 package com.moodanalyzertest;
 
 public class MoodAnalyzer {
+
+    public String analyzeMood(String message) {
+        if (message.contains("I am in sad Mood"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
--- src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
@@ -1,6 +1,13 @@
 package com.moodanalyzertest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String moodSad = moodAnalyzer.analyzeMood("I am in sad Mood");
+        Assert.assertEquals("SAD", moodSad);
+    }
 }
