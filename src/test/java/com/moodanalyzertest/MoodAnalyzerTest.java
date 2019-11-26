99de0aba8f7c4c638d3cb3645a71ceb24ca29ca2
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-26 10:49:51 +0530

    REFACTOR: functionality to take the mood message in constructor and respond to HAPPY

--- src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
@@ -9,4 +9,10 @@ public class MoodAnalyzerTest {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad Mood");
         Assert.assertEquals("SAD", moodAnalyzer.analyzeMood());
     }
+
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
+        Assert.assertEquals("HAPPY", moodAnalyzer.analyzeMood());
+    }
 }
